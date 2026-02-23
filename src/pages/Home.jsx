2a087ec4817ef142455bb9705b68fc492ab43e9f
Cyril Tagoe <96398158+Cyril-Nii@users.noreply.github.com>
2026-02-23 17:40:48 +0000

    add useDocumentTitle hook and set document title in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import useDocumentTitle from "../hooks/useDocumentTitle.js";
+
 export const Home = () => {
+
+    useDocumentTitle('Coinbase - Buy and Sell Bitcoin, Ethereum, and more with trust');
+
     return (
-        <></>
+        <p className={"text-3xl text-blue-900"}>Welcome to the Homepage</p>
     )
 }
