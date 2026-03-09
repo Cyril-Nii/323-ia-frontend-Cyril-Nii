2b6ea9467ec223467de8f26c534f0898f7dd66c3
Cyril Tagoe <96398158+Cyril-Nii@users.noreply.github.com>
2026-03-09 14:29:43 +0000

    refactor App component to use default exports for pages and clean up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import {Home} from "./pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 import SignIn from './pages/SignIn.jsx';
-// import SignUp from './pages/SignUp.jsx';
+import SignUp from './pages/SignUp.jsx';
 // import AccountTypeSelect from './pages/AccountTypeSelect';
 // import ForgotPassword from './pages/ForgotPassword';
 // import VerifyCode from './pages/VerifyCode';
-// import ExplorePage from './pages/ExplorePage';
+import Explore from './pages/Explore';
 // import MarketStatsPage from './pages/MarketStatsPage';
-// import LearnPage from './pages/LearnPage';
+import LearnPage from './pages/Learn.jsx';
 // import CryptoBasicsPage from './pages/CryptoBasicsPage';
 import Loader from './components/common/Loader.jsx';
 
@@ -28,14 +28,14 @@ const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            {/*<Route path="/explore" element={<ExplorePage />} />*/}
+            <Route path="/explore" element={<Explore />} />
             {/*<Route path="/market-stats" element={<MarketStatsPage />} />*/}
             <Route path="/signin" element={<SignIn />} />
             {/*<Route path="/account-type" element={<AccountTypeSelect />} />*/}
-            {/*<Route path="/signup" element={<SignUp />} />*/}
+            <Route path="/signup" element={<SignUp />} />
             {/*<Route path="/forgot-password" element={<ForgotPassword />} />*/}
             {/*<Route path="/verify" element={<VerifyCode />} />*/}
-            {/*<Route path="/learn" element={<LearnPage />} />*/}
+            <Route path="/learn" element={<LearnPage />} />
             {/*<Route path="/learn/crypto-basics" element={<CryptoBasicsPage />} />*/}
         </Routes>
     )
