4cabb9a6921c18423ef09cce1848327f4ce34ca0
Cyril Tagoe <96398158+Cyril-Nii@users.noreply.github.com>
2026-03-09 14:27:13 +0000

    refactor Learn component to utilize external data and improve structure

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,12 +1,25 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { PlayCircle } from 'lucide-react';
 import Header from '../components/layout/Header.jsx';
 import Footer from '../components/layout/Footer.jsx';
 import SubscribePopup from '../components/common/SubscribePopup.jsx';
-import LearnCard from '../components/LearnCard';
-import { PlayCircle } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import LearnCard from '../components/cards/LearnCard.jsx';
+import {
+    popularArticles,
+    cryptoBasics,
+    whatIs,
+    tipsAndTutorials,
+    advancedTrading,
+    futures,
+    allThingsWallet
+} from '../data/learnData.js';
+import useDocumentTitle from "../hooks/useDocumentTitle.js";
 
-export default function LearnPage() {
+const Learn = () => {
+
+    useDocumentTitle('Coinbase Learn - Your crypto questions, answered');
+
     const [showPopup, setShowPopup] = useState(true);
 
     // Lock scroll when popup is open
@@ -22,70 +35,6 @@ export default function LearnPage() {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-
-    const popularArticles = [
-        { title: 'What is cryptocurrency?', label: 'Beginner\'s Guide' },
-        { title: 'How to earn crypto rewards', label: 'Getting Started' },
-        { title: 'How to add crypto to your Coinbase Wallet', label: 'Getting Started' },
-        { title: 'Tax forms, explained: A guide to U.S. tax forms and crypto reports', label: 'Your crypto' },
-        { title: 'Beginner’s guide to dapps', label: 'Getting Started' },
-        { title: 'Everything you need to know about the first-ever U.S. Bitcoin ETF', label: 'Market Update' }
-    ];
-
-    const cryptoBasics = [
-        { label: "Beginner's Guide", title: "What is Bitcoin?", description: "Bitcoin is the world's first widely adopted cryptocurrency — it allows for secure and seamless peer-to-peer transactions on the internet.", image: "https://images.ctfassets.net/q5ulk4bp65r7/lUIdMeDm9tf33LZNjPqz8/a44f28b20bd9846efc62cf5a230d875a/Learn_Illustration_Ultimate_Guide_Bitcoin.webp?w=768&fm=png", href: "#" },
-        { label: "Beginner's Guide", title: "Guide to DeFi tokens and altcoins", description: "From Aave to Zcash, decide what to trade with our beginner's guide", image: "https://images.ctfassets.net/q5ulk4bp65r7/3rv8jr1B1Z1dZ2EhHqo7dp/e74ddbf1cd4836b83d34fe5cec351d78/Alt-Coin.png?w=768&fm=png", href: "#" },
-        { label: "Beginner's guide", title: "What is Ethereum?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/3thWklmvu2WmAHJh0k1AcC/51521feeef170d94a446fbec6f262912/what-is-ethereum.png?w=768&fm=png", href: "#" },
-        { label: "Key term", title: "What is DeFi?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/2lrWtXLcleZPbsnzZnEeLB/bbd5a35075619f07e083fce5fdbf15f9/Learn_Illustration_What_is_DeFi.jpg?w=768&fm=png", href: "#" },
-        { label: "Beginner's Guide", title: "What is a stablecoin?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/3hETt7h2hfvnOnVVrJIvlO/b7204c2b9a1a35d39d0dd396d2cf49bb/Learn_Illustration_What_is_a_stablecoin.jpg?w=768&fm=png", href: "#" },
-        { label: "Glossary", title: "Don’t let FUD give you FOMO or you’ll end up REKT — crypto slang, explained", description: "Don’t let FUD give you FOMO or you’ll end up REKT — crypto slang, explained", image: "https://images.ctfassets.net/q5ulk4bp65r7/5fZ31B0CLFBDfIWK3DQPTN/b98e564a067cbb252995d654006cee09/Group_31612615.png?w=768&fm=png", href: "#" },
-    ];
-
-    const whatIs = [
-        { label: "Bitcoin", title: "What is..." },
-        { label: "Blockchain", title: "What is..." },
-        { label: "Cardano", title: "What is..." },
-        { label: "Crypto wallet", title: "What is..." },
-        { label: "DeFi", title: "What is..." },
-        { label: "Ethereum", title: "What is..." },
-        { label: "Fork", title: "What is..." },
-        { label: "Inflation", title: "What is..." },
-        { label: "Market cap", title: "What is..." },
-        { label: "NFT", title: "What is..." },
-        { label: "Private key", title: "What is..." },
-        { label: "Protocol", title: "What is..." },
-        { label: "Smart contract", title: "What is..." },
-        { label: "Token", title: "What is..." },
-        { label: "Volatility memecoin", title: "What is..." }
-    ];
-
-    const tipsAndTutorials = [
-        { label: "Getting Started", title: "How to donate crypto", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/63o0Mbwyiqcqq8CLZKToLs/4d007f0923a20999c6c4765d6fdc35bf/Donating-Crypto.png?w=768&fm=png", href: "#" },
-        { label: "Video Tutorial", title: "How to set up a crypto wallet", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/5wgZmGhDLxwejh5MDDxRAn/aa73d7119d45e95ab417b9ae5e5e8f56/Video_02.png?w=768&fm=png", href: "#" },
-        { label: "Video Tutorial", title: "When is the best time to invest in crypto?", description: "Learn how to setup and get started with a crypto wallet.", image: "https://images.ctfassets.net/q5ulk4bp65r7/3K4qo02ZA5PkwyN5Rm7gjm/945bce812fc91da9ef737516142eb281/Dollar-Cost_avg.png?w=768&fm=png", href: "#" },
-        { label: "Your crypto", title: "How to invest in crypto via your retirement account", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/5Crc40l3fe9Mm22C4HGRgx/71defd6861e1ee2c4927e709604ea099/Crypto___Retirement_Op2-D.png?w=768&fm=png", href: "#" },
-    ];
-
-    const advancedTrading = [
-        { label: "Key term", title: "What is technical analysis?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/50bz6qkc8hSzqqHhbiMtKb/1e4d3f35ff2cd049580a4eb20f861a6e/Learn_Illustration_What_is_Technical_Analysis__1_.png?w=768&fm=png", href: "#" },
-        { label: "Advanced Guide", title: "How can I use crypto futures market data for spot trading?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/4zm6shyv9LYK0W74kUrMQ4/2ecc82c8215405ecfba08f1f652095a6/charting-indicators.png?w=768&fm=png", href: "#" },
-        { label: "Advanced guide", title: "How to read advanced trading charts", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/1As6H6C4F2KcJsyLzzWPgX/7b13a011336a72f1f90f5d87bf32eee7/advance-trading.png?w=768&fm=png", href: "#" },
-        { label: "Key term", title: "What is an order book?", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/4q6K5epqo9YLwaQ79i1M5N/9693bdbae13fffb3c37406d71f294244/order-book__1_.png?w=768&fm=png", href: "#" }
-    ];
-
-    const futures = [
-        { label: "Futures", title: "Futures: Introductions and origins", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/7mOyQ8m1ax3GFyzTSIENQy/2977ec18240fcc1851eb4a460a5915c3/Article_1_-_Visual_-_1.png?w=768&fm=png", href: "#" },
-        { label: "Futures", title: "Futures fundamentals: Understanding the basics", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/P1YwgqZX7jfnJ0l1xdbED/7a1d8654bf047462d68a377f7d21f39c/Article_2_-_Visual_-_1.png?w=768&fm=png", href: "#" },
-        { label: "Futures", title: "Opening, holding, and closing a position in the futures market", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/3REMTvIb9OoMmr8EPya1zU/f06976781a368ab76f5fa4b3edfdcc66/Article_3_-_Visual_-_1.png?w=768&fm=png", href: "#" },
-        { label: "Futures", title: "Trading strategies: Speculating, hedging, and spreading in the futures market", description: "", image: "https://images.ctfassets.net/q5ulk4bp65r7/EnVSYIBpxhx5Aicjq3A8f/08d26b8be710727725c2523746a710de/Article_4_-_Visual_-_1.png?w=768&fm=png", href: "#" }
-    ];
-
-    const allThingsWallet = [
-        { label: "Wallet", title: "What’s the difference between Coinbase and Coinbase Wallet?", description: "And how can a wallet help me access NFTs or DeFi? Your self-custody wallet questions, answered", image: "https://images.ctfassets.net/q5ulk4bp65r7/3mfq3iIdYW3CDfWoh5vm9j/e79252903418650aa29f66ee313ef44e/How_to_Coinbase_Wallet_Op1-B.png?w=768&fm=png", href: "#" },
-        { label: "Video Tutorial", title: "How to set up a crypto wallet", description: "Learn how to setup and get started with a crypto wallet.", image: "https://images.ctfassets.net/q5ulk4bp65r7/5wgZmGhDLxwejh5MDDxRAn/aa73d7119d45e95ab417b9ae5e5e8f56/Video_02.png?w=768&fm=png", href: "#" },
-        { label: "Getting Started", title: "How to add crypto to your Coinbase Wallet", description: "A quick guide on how to add crypto to your Coinbase self-custody wallet.", image: "https://images.ctfassets.net/q5ulk4bp65r7/3G50jPNvtkBsSz7we9TaxK/a50e3a2103bad2af0355644349e57476/how_to_fund_coinbase_wallet.png?w=768&fm=png", href: "#" },
-        { label: "Wallet", title: "How to send or receive crypto using Coinbase Wallet", description: "Coinbase Wallet helps you unlock one of the most significant features of crypto: the ability to send or receive peer-to-peer transfers without any financial intermediaries.", image: "https://images.ctfassets.net/q5ulk4bp65r7/4ktmMla57tgU3ZYleBOlLm/71ef298196ffcf1d1eb6d2b43abe49d4/Learn_Illustration_How_to_Send_Crypto.png?w=768&fm=png", href: "#" }
-    ];
 
     return (
         <div className="min-h-screen flex flex-col bg-white">
@@ -283,3 +232,5 @@ export default function LearnPage() {
         </div>
     );
 }
+
+export default Learn;
